0f7f06d9466e2833eebdcb77232a55f9c172dd4e
sesha932000 <sesha932000@gmail.com>
2024-12-22 06:25:17 +0530

    first commit

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    protected $student;
+    public function __construct(){
+        $this->student = new Student();
+    }
+
+    //http://127.0.0.1:8000/api/student
+    public function index()
+    {
+        return $this->student->all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return $this->student->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+
+     return $student = $this->student->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $student = $this->student->find($id);
+        $student->update($request->all());
+        return $student;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $student = $this->student->find($id);
+        return $student->delete();
+    }
+}
